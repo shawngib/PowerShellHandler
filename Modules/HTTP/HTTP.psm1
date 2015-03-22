a23167699dfd342c13c13bb73313a82fcbb52ead
mgreenegit <migreene@outlook.com>
2015-03-22 16:53:38 -0500

    Adding HTTP module and one page sample (no content)

---
+++ Modules/HTTP/HTTP.psm1
@@ -0,0 +1,28 @@
+﻿function Add-Title ($text, $style = 'h1'){
+"<$style>$text</$style>"
+}
+
+function Add-HorizontalRule {
+'<hr>'
+}
+
+function Add-Image ($source, $size) {
+"<img src=`"$source`" width=$size>"
+}
+
+function Add-Text ($text, $style) {
+"<p style=`"$style`">$text</p>"
+}
+
+function Add-Head ($title, $link) {
+"<title>$title</title>"
+"<link rel=`"stylesheet`" href=`"$link`" type=`"text/css`">"
+}
+
+function Add-DIV ($id, $content) {
+"<div id=`"$id`">$content</div>"
+}
+
+function Add-Link ($url, $text) {
+"<a href=`"$url`">$text</a>"
+}
---
+++ PageSamples/PhotosFromMyVacation.ps1
@@ -0,0 +1,23 @@
+﻿$folder = (resolve-path 'c:\inetpub\posh\vacation').Path
+
+$bgcolor = 'tan'
+$TopTextFont = 'verdana'
+$TopText = 'Photos from my Vacation!'
+
+foreach ($photo in (get-childitem $folder | % Name)) {
+    $Table += "<td><img src=`"./vacation/$photo`" width=300></td>"
+}
+
+
+
+"<h2><font face=$TopTextFont>$TopText<font></h2>
+<body bgcolor = $bgcolor>
+<hr>
+<table>
+   <tr>
+       $Table
+   </tr>
+</table>
+<hr>
+</body>
+"
